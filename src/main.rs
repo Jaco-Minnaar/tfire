749c78d3bec761d95240796202a078409b737f1c
Jaco Minnaar <jaco@jacominnaar.com>
2022-08-21 14:17:55 +0200

    Added another thread to split calculation and writing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use lazy_static::lazy_static;
 use rand::Rng;
 use std::{
     io::{stdin, stdout, Read, Stdout, StdoutLock, Write},
-    sync::mpsc,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        mpsc::{self, Receiver, Sender},
+        Arc, Mutex,
+    },
     thread,
     time::Duration,
 };
@@ -57,21 +61,55 @@ lazy_static! {
 
 fn main() {
     let (stop_tx, stop_rx) = mpsc::channel();
-    let handle = thread::spawn(move || {
-        let mut terminal = Terminal::default();
-        loop {
-            terminal.next_frame();
 
-            match stop_rx.try_recv() {
-                Ok(false) => terminal.stop_flames(),
-                Ok(true) => break,
-                Err(_) => (),
+    let handle = thread::spawn(move || {
+        let (signal_tx, signal_rx) = mpsc::channel();
+        let (buffer_tx, buffer_rx) = mpsc::channel();
+        let (stop_tx_2, stop_rx_2) = mpsc::channel();
+        let is_done = Arc::new(AtomicBool::new(false));
+        let is_done_clone = Arc::clone(&is_done);
+
+        let mut terminal = Terminal::new(buffer_rx, signal_tx);
+
+        let width = terminal.width;
+        let height = terminal.height;
+
+        let draw_handle = thread::spawn(move || {
+            let mut buffer_container = BufferContainer::new(width, height, buffer_tx, signal_rx);
+            loop {
+                buffer_container.next_frame();
+
+                match stop_rx_2.try_recv() {
+                    Ok(true) => break,
+                    Ok(false) => buffer_container.stop_flames(),
+                    Err(_) => (),
+                }
+
+                if buffer_container.done {
+                    is_done.store(true, Ordering::Relaxed);
+                    break;
+                }
+
+                thread::sleep(Duration::from_millis(20));
+            }
+        });
+
+        loop {
+            terminal.draw();
+
+            if let Ok(val) = stop_rx.try_recv() {
+                stop_tx_2.send(val).unwrap();
+                if val {
+                    break;
+                }
             }
 
-            if terminal.done {
+            if is_done_clone.load(Ordering::Relaxed) {
                 break;
             }
         }
+
+        draw_handle.join().unwrap();
     });
 
     let key = stdin().lock().keys().next().unwrap().unwrap();
@@ -85,67 +123,65 @@ fn main() {
     handle.join().unwrap();
 }
 
-struct Terminal<W: Write> {
+struct BufferContainer {
     width: usize,
     height: usize,
-    read_frame_buffer: Vec<usize>,
     write_frame_buffer: Vec<usize>,
-    out: W,
     stop: bool,
+    sender: Sender<Vec<usize>>,
+    signal: Receiver<()>,
     pub done: bool,
 }
 
-impl<W: Write> Terminal<W> {
-    fn draw(&mut self) {
-        let mut has_color = false;
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let new_index = self.write_frame_buffer[y * self.width + x];
-                let index = self.read_frame_buffer[y * self.width + x];
-
-                let color = COLORS[index];
-                let new_color = COLORS[new_index];
-
-                if !has_color && index != 0 {
-                    has_color = true;
-                }
-
-                if color == new_color {
-                    continue;
-                }
-
-                write!(
-                    self.out,
-                    "{}{}█",
-                    cursor::Goto((x + 1) as u16, (y + 1) as u16),
-                    color::Fg(new_color)
-                )
-                .unwrap();
-            }
+impl BufferContainer {
+    pub fn new(
+        width: usize,
+        height: usize,
+        sender: Sender<Vec<usize>>,
+        signal: Receiver<()>,
+    ) -> Self {
+        let mut frame_buffer = vec![0; width * height];
+        let bottom_row = (width * (height - 1)) as usize;
+        for i in bottom_row..frame_buffer.len() {
+            frame_buffer[i] = 35;
         }
-
-        self.out.flush().unwrap();
-
-        self.read_frame_buffer = self.write_frame_buffer.clone();
-
-        if !has_color {
-            self.done = true;
+        Self {
+            width,
+            height,
+            sender,
+            signal,
+            write_frame_buffer: frame_buffer,
+            stop: false,
+            done: false,
         }
     }
 
     fn next_frame(&mut self) {
         let mut rng = rand::thread_rng();
+
+        if self.stop {
+            self.cool_flames();
+        }
+
         for x in 0..self.width {
             for y in 1..self.height {
                 self.spread_fire(y * self.width + x, rng.gen());
             }
         }
 
-        self.draw();
-
-        if self.stop {
-            self.cool_flames();
+        if let Ok(_) = self.signal.try_recv() {
+            self.sender.send(self.write_frame_buffer.clone()).unwrap();
         }
+
+        let mut all_black = true;
+        for index in &self.write_frame_buffer {
+            if index != &0 {
+                all_black = false;
+                break;
+            }
+        }
+
+        self.done = all_black;
     }
 
     fn spread_fire(&mut self, from: usize, coef: f32) {
@@ -175,8 +211,17 @@ impl<W: Write> Terminal<W> {
     }
 }
 
-impl Default for Terminal<HideCursor<RawTerminal<StdoutLock<'_>>>> {
-    fn default() -> Self {
+struct Terminal<W: Write> {
+    pub width: usize,
+    pub height: usize,
+    pub frame_buffer: Vec<usize>,
+    out: W,
+    signal_tx: Sender<()>,
+    buffer_rx: Receiver<Vec<usize>>,
+}
+
+impl Terminal<HideCursor<RawTerminal<StdoutLock<'static>>>> {
+    pub fn new(buffer_rx: Receiver<Vec<usize>>, signal_tx: Sender<()>) -> Self {
         let (width, height) = termion::terminal_size().unwrap();
 
         let width = width as usize;
@@ -194,15 +239,48 @@ impl Default for Terminal<HideCursor<RawTerminal<StdoutLock<'_>>>> {
         let mut terminal = Self {
             width,
             height,
-            read_frame_buffer: frame_buffer.clone(),
-            write_frame_buffer: frame_buffer,
+            buffer_rx,
+            signal_tx,
+            frame_buffer: frame_buffer.clone(),
             out: writer,
-            stop: false,
-            done: false,
         };
 
-        terminal.draw();
-
         terminal
+    }
+}
+
+impl<W: Write> Terminal<W> {
+    fn draw(&mut self) {
+        if let Err(_) = self.signal_tx.send(()) {
+            return;
+        }
+
+        let write_frame_buffer = self.buffer_rx.recv().unwrap();
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let new_index = write_frame_buffer[y * self.width + x];
+                let index = self.frame_buffer[y * self.width + x];
+
+                let color = COLORS[index];
+                let new_color = COLORS[new_index];
+
+                if color == new_color {
+                    continue;
+                }
+
+                write!(
+                    self.out,
+                    "{}{}█",
+                    cursor::Goto((x + 1) as u16, (y + 1) as u16),
+                    color::Fg(new_color)
+                )
+                .unwrap();
+            }
+        }
+
+        self.out.flush().unwrap();
+
+        self.frame_buffer = write_frame_buffer.clone();
     }
 }
